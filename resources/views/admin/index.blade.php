8fd6f8e4cd074c8b018c947ce4e35c67392e6c7d
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-02-27 15:23:50 +0100

    add admin pages

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.admin')
+
+@section('content')
+    <p>Welcome to the admin interface!</p>
+@endsection
---
+++ routes/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthorController;
+use App\Http\Controllers\Admin\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+
+// Route::middleware(['auth'])->group(function() {
+
+
+    Route::get('/', function () {
+        return view('admin.index');
+    })->name('admin.index');
+ 
+    Route::get('/authors', [AuthorController::class, 'index'])->name('admin.author.index');
+    Route::get('/authors/create', ['App\Http\Controllers\Admin\AuthorController', 'create'])->name('admin.author.create');
+    Route::post('/authors/store', ['App\Http\Controllers\Admin\AuthorController', 'store'])->name('admin.author.store');
+    
+    Route::get('/books', [BookController::class, 'index'])->name('admin.book.index');
+    Route::get('/books/create', ['App\Http\Controllers\Admin\BookController', 'edit'])->name('admin.book.create');
+    Route::get('/books/edit/{id}', ['App\Http\Controllers\Admin\BookController', 'edit'])->name('admin.book.edit');
+    Route::post('/books/store', ['App\Http\Controllers\Admin\BookController', 'store'])->name('admin.book.store');
+    Route::put('/books/update/{id?}', ['App\Http\Controllers\Admin\BookController', 'store'])->name('admin.book.update');
+   
+// });
